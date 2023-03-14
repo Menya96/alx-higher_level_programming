492582e86f3e8866274088ffb050093a23ba9750
Menya96 <menyahtonny@gmail.com>
2023-03-14 04:05:38 +0300

    success

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!usr/bin/pyhton3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+    
+    length = len(my_list)
+    
+    if idx > length - 1:
+        return (my_list)
+    
+    my_list.pop(idx)
+    my_list.insert(idx, element)
+    return (my_list)
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,3 @@
+
+This is the third project in higher level programming repository.Data structures:Iterables
+Lists and Tuples
