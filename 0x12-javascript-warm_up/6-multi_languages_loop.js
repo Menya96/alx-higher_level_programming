7176de9ecd3ece3ac104afcbb1ba2fcdc5db320d
Menya96 <menyahtonny@gmail.com>
2024-04-23 22:57:55 +0300

    value arguement added

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 // ['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach((e) => console.log(e));
 
-const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-msg.forEach((element) => console.log(element))
+msg.forEach((element) => console.log(element));
