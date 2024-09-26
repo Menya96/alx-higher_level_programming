e45a563fc33b777f9adfa2e112fdf579bf0289db
Menya96 <menyahtonny@gmail.com>
2024-09-26 08:08:58 -0700

    error fix in tests

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-  constructor(w, h) {
-    this.width = w;
-    this.height = h;
+/**
+ * Check the parameters provided
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
-};
+}
+module.exports = Rectangle;
